5262f58e5a21b27c9b93944608a3d38641a1cbf7
chaLyoung <dreamlovinghigh57@gmail.com>
2022-10-25 19:26:24 +0900

    add database config yml file

--- src/main/java/com/dev/springboot_database/SpringbootDatabaseApplication.java
+++ src/main/java/com/dev/springboot_database/SpringbootDatabaseApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootDatabaseApplication {
 
     public static void main(String[] args) {
+        String profile = StringUtil.nvl(System.getProperty("spring.profiles.active"), "local");
+        System.setProperty("spring.profiles.active", profile);
+        System.setProperty("service.name", "springboot_database");
         SpringApplication.run(SpringbootDatabaseApplication.class, args);
     }
 
